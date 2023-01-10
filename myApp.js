262f2ec3a5a9b0b67af93253edd7a1f566752837
Daniel Dip <dani.dip.2@gmail.com>
2023-01-10 15:05:54 -0300

    express env

--- myApp.js
+++ myApp.js
@@ -1,10 +1,25 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", (_req, res) =>{
+  // res.send("Hello Express")
+  absolutePath = __dirname + '/views/index.html'
+  res.sendFile(absolutePath)
+})
 
+app.get("/json", (req, res) =>{
+  let obj = {"message": "Hello json"}
+    if(process.env.MESSAGE_STYLE == 'uppercase'){
+        obj.message = obj.message.toUpperCase()
+        res.json(obj)
+    }else{
+        res.json(obj)
 
-
+    }
+})
 
 
 
