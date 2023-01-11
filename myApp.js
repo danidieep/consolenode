e0aea364949e6283c5a8fe456b883eb991867faa
Daniel Dip <dani.dip.2@gmail.com>
2023-01-11 20:08:20 -0300

    finish

--- myApp.js
+++ myApp.js
@@ -1,8 +1,23 @@
 let express = require('express');
 let app = express();
 require('dotenv').config()
+let bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.use("/public", express.static(__dirname + "/public"));
+
+app.get('/now', (req, res, next) =>{
+
+  req.time = new Date().toString()
+  next()  
+},(req, res)=>{
+  let obj = {}
+  obj.time = req.time
+  res.json(obj)
+})
+
+
 
 app.get("/", (_req, res) =>{
   // res.send("Hello Express")
@@ -21,6 +36,27 @@ app.get("/json", (req, res) =>{
     }
 })
 
+app.get('/:word/echo', (req, res, next)=>{
+  let {word} = req.params
+  let obj = {}
+  obj.echo = word
+  res.json(obj)
+})
+
+app.get('/name', (req, res, next) =>{
+  let {first, last} = req.query
+  let obj = {}
+  obj.name = `${first} ${last}`
+  res.json(obj)
+})
+
+
+app.post('/name', (req, res, next) =>{
+  let {first, last} = req.body
+  let obj={}
+  obj.name = `${first} ${last}`
+  res.json(obj)
+})
 
 
 
